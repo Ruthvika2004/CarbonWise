014e50437f44d1781e321b53e669c277b218d10a
ruthujain <114241175+ruthujain@users.noreply.github.com>
2024-11-15 20:00:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 require('dotenv').config()
 const express=require('express')
+
+const cors=require('cors')
+
 const mongoose=require('mongoose')
 const  projectRoutes = require('./routes/projects')
+
 
 // starting/ creating express app
 const app=express()
 
+//ruthvika
+const routes = require('./routes/routes'); // Import the routes file
+
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use('/', routes);
+//ruthvika
 
 
 // middleware
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
