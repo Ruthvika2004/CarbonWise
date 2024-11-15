c6b4bebc82eb109f371aed2e3708bb4637b5653c
ruthujain <114241175+ruthujain@users.noreply.github.com>
2024-11-15 09:37:05 +0530

    Add files via upload
    added all backend files for the project listing feature thats finished so far (except node_moules)

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express=require('express')
+const mongoose=require('mongoose')
+const  projectRoutes = require('./routes/projects')
+
+// starting/ creating express app
+const app=express()
+
+
+
+// middleware
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+  })
+
+// routes
+app.use('/api/projects', projectRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+  //listen foir requests
+app.listen(process.env.PORT,()=>{
+  console.log(' conncected to db, listening!!',process.env.PORT)
+})
+
+
+})
+.catch((error)=>{
+  console.log(error)
+})
+
+
+
